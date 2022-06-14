b1dd49e2da37bd13807efa9914bc02ad81cfacdb
An7112 <thanhan7112@gmail.com>
2022-06-14 12:34:43 +0700

    update

---
+++ project/src/Page/Main.js
@@ -0,0 +1,16 @@
+import "./Main.css";
+
+function Main() {
+  return (
+    <div className="main">
+      <div className="MenuMain">
+        <div className="Logo"></div>
+        <div className="MenuHome">
+            <h3>Home</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
